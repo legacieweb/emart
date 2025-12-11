7a8117e6205dc1b60b8b3ea115597b7fb3601a25
iyonicorp <hello@iyonicorp.com>
2025-12-11 14:22:08 +0300

    L

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/emart';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://iyonicorp:iyonicorp@iyonicweb.ypgpsxv.mongodb.net/?retryWrites=true&w=majority&appName=iyonicweb';
 
 app.use(cors());
 app.use(express.json());
